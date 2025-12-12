ef454be6bc6b1b917f11af7effd4f6b0d0551384
Nikita Rohit Gupta <omarrohit20@gmail.com>
2025-12-12 19:53:11 +0530

    Update .gitignore and modify config.json for environment settings; refactor shippingLabel.spec.ts to use dataset directly

--- tests/shippingLabel.spec.ts
+++ tests/shippingLabel.spec.ts
@@ -1,33 +1,7 @@
 import { test } from '@playwright/test';
-import dataset from '../data/tasks.json';
 import { LoginPage } from '../pages/LoginPage';
 import { ShippingPage } from '../pages/ShippingPage';
-
-type TaskData = {
-  id: string;
-  app: string;
-  task: string;
-  tags: string[];
-  name: string,
-  company: string,
-  addressLine1: string,
-  addressLine2: string,
-  zip: string,
-  city: string,
-  state: string,
-  email: string,
-  phone: string,
-  package: string,
-  length: string,
-  width: string,
-  height: string,
-  weightlb: string,
-  weightoz: string,
-  service: string,
-  splService: string
-};
-
-const tasks: TaskData[] = dataset as TaskData[];
+import dataset from '../data/tasks.json';
 
 test.beforeEach(async ({ page }) => {
   const loginPage = new LoginPage(page);
@@ -36,7 +10,7 @@ test.beforeEach(async ({ page }) => {
   await loginPage.navigateToHomePage();
 });
 
-for (const t of tasks) {
+for (const t of dataset) {
   test(`${t.id} - Verify test "${t.task}" in ${t.app}`, async ({ page }) => {
     const shippingPage = new ShippingPage(page);
     await shippingPage.fillRecipientAddress(t.name, t.company, t.addressLine1, t.addressLine2, t.zip, t.city, t.state, t.email, t.phone);
